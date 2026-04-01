8e93dac828454bad85a12883cbd5d53f5d2c6f5e
가동재 <masterforce999@naver.com>
2026-04-01 20:02:09 +0900

    업데이트

--- src/components/slider-item/slider-item03.tsx
+++ src/components/slider-item/slider-item03.tsx
@@ -2,19 +2,19 @@ import Image from "next/image";
 
 const R2_URL = process.env.NEXT_PUBLIC_R2_PUBLIC_URL;
 
-export default function SliderItem01() {
+export default function SliderItem03() {
   return (
     <div className="relative w-full h-full">
       <Image
-        src={`${R2_URL}/chuncheon/pk_2.png`}
-        alt="춘천2"
+        src={`${R2_URL}/chuncheon/pk_4.png`}
+        alt="춘천4"
         fill
         className="object-fill min-[1080px]:hidden block"
         priority
       />
       <Image
-        src={`${R2_URL}/chuncheon/pk_2.png`}
-        alt="춘천2"
+        src={`${R2_URL}/chuncheon/pk_4.png`}
+        alt="춘천4"
         fill
         className="object-fill hidden min-[1080px]:block"
         priority
