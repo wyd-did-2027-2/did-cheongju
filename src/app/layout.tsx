ae3eb284550858137251a88085fdb6eba74a56d5
가동재 <masterforce999@naver.com>
2026-06-04 18:32:04 +0900

    Update Cheongju root layout class

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,7 +58,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko" className="lenis lenis-smooth">
+    <html lang="ko" className="lenis lenis-smooth test">
       <body className={pretendard.className}>{children}</body>
     </html>
   );
